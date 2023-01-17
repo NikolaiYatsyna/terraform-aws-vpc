1b191100672bd638af2007198c7e816304ed879a
Nikolai <nikolai.yatsyna@gmail.com>
2023-01-17 22:06:10 +0400

    fix AZ count (#3)

--- vpc.tf
+++ vpc.tf
@@ -10,15 +10,15 @@ locals {
   available_zones_count = length(data.aws_availability_zones.available.names)
   zone_count      = var.zone_count < local.available_zones_count ? var.zone_count : local.available_zones_count
   range           = cidrsubnets(var.cidr, [for i in range(local.available_zones_count * 2) : 8]...)
-  private_subnets = slice(local.range, 0, local.zone_count - 1)
-  public_subnets  = reverse(slice(reverse(local.range), 0, local.zone_count - 1))
+  private_subnets = slice(local.range, 0, local.zone_count)
+  public_subnets  = reverse(slice(reverse(local.range), 0, local.zone_count))
 }
 
 module "vpc" {
   source               = "terraform-aws-modules/vpc/aws"
   name                 = var.name
   cidr                 = var.cidr
-  azs                  = slice(data.aws_availability_zones.available.names, 0, local.zone_count - 1)
+  azs                  = slice(data.aws_availability_zones.available.names, 0, local.zone_count)
   private_subnets      = local.private_subnets
   public_subnets       = local.public_subnets
   enable_nat_gateway   = true
